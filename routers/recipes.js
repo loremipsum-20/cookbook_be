20527ab4122165280f76b548883ca5a4dcf0c2a3
Lena <l.ernst3498@gmail.com>
2021-06-26 16:07:16 +0200

    add error handling to get request, make post request

--- routers/recipes.js
+++ routers/recipes.js
@@ -1,31 +1,43 @@
-const express = require('express')
-const router = express.Router()
-const RecipeSchema = require('../models/RecipeModel')
+const express = require("express");
+const router = express.Router();
+const RecipeSchema = require("../models/RecipeModel");
 
-router.post('/', async (req, res) => {
-  // manually inserted new Entry - we don't want this most of the time
-  // const student = {
-  //   name: 'Gio',
-  //   first_name: 'me',
-  //   email: 'gio@gio.com',
-  // }
+// router.post("/", async (req, res) => {
+//   // manually inserted new Entry - we don't want this most of the time
+//   // const student = {
+//   //   name: 'Gio',
+//   //   first_name: 'me',
+//   //   email: 'gio@gio.com',
+//   // }
 
-  const recipe = req.body
+//   const recipe = req.body;
 
-  // you can use await/async with Try/catch block to catch error
-  // or you can use promises with .then / .catch
-  try {
-    const newRecipe = await RecipeSchema.create(recipe)
-    console.log(newRecipe)
-  } catch (err) {
-    console.log('err creating new recipe', err)
-  }
-})
+//   // you can use await/async with Try/catch block to catch error
+//   // or you can use promises with .then / .catch
+//   try {
+//     const newRecipe = await RecipeSchema.create(recipe);
+//     console.log(newRecipe);
+//   } catch (err) {
+//     console.log("err creating new recipe", err);
+//   }
+// });
 
-router.get('/', (req, res) => {
-  RecipeSchema.find({}, (err, data) => res.send(data))
+router.post("/", async (req, res) => {
+  const recipe = new RecipeSchema({
+    slug: req.body.slug,
+    shortDescription: req.body.shortDescription,
+    ingredientsList: req.body.ingredientsList,
+    time: req.body.time,
+    description: req.body.description,
+    category: req.body.category,
+  });
+  await recipe.save();
+  res.send(recipe);
+});
 
-})
+router.get("/", (req, res) => {
+  RecipeSchema.find({}, (err, data) => res.send(data));
+});
 //get all recipes
 //router.get("/api/recipes", (req, res, next) => {
 //  res.json(data.entries);
@@ -33,8 +45,23 @@ router.get('/', (req, res) => {
 
 //get 1 recipe
 //router.get("/:id", (req, res, next) => {
-  //console.log(data[req.params.id]);
-  //res.send(data);
+//console.log(data[req.params.id]);
+//res.send(data);
 //});
 
-module.exports = router
+// router.get("/:slug", async (req, res) => {
+//   const recipe = await RecipeSchema.findOne({ slug: req.params.slug });
+//   res.send(recipe);
+// });
+
+router.get("/:slug", async (req, res) => {
+  try {
+    const recipe = await RecipeSchema.findOne({ slug: req.params.slug });
+    res.send(recipe);
+  } catch {
+    res.status(404);
+    res.send({ error: "Recipe doesn't exist!" });
+  }
+});
+
+module.exports = router;
