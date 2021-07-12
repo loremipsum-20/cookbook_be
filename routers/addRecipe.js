163fe1d48c00809125e904659bbb426100222034
Lena <l.ernst3498@gmail.com>
2021-07-12 23:10:12 +0200

    delete redundant file

--- routers/addRecipe.js
+++
@@ -1,7 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const RecipeSchema = require("../models/RecipeModel");
-
-// router.post("/addRecipe", async (req, res) => {
-
-// };
